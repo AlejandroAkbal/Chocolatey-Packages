95c144269e3db2f26a3451307977a5ea6bb8041f
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-12-12 22:10:25 +0000

    feat: add hoppscotch

---
+++ hoppscotch/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = 'https://github.com/hoppscotch/releases/releases/download/v23.8.4-1/Hoppscotch_win_x64.msi'
+
+  softwareName  = 'Hoppscotch*'
+
+  checksum      = 'F2D8D6A8195AE1AD517B4DBF397BC1551335EE6F25AEBBE83140BB8815084F75'
+  checksumType  = 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
